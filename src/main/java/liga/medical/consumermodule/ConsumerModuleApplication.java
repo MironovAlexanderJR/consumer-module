35bae8aab90dd93f73f2987d72d8c6bda2a81737
Miron <alexander.mironov.jr@ya.ru>
2022-10-13 20:45:38 +0300

    add RabbitMQ

---
+++ src/main/java/liga/medical/consumermodule/ConsumerModuleApplication.java
@@ -0,0 +1,13 @@
+package liga.medical.consumermodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerModuleApplication.class, args);
+	}
+
+}
---
+++ src/main/java/liga/medical/consumermodule/model/MessageType.java
@@ -0,0 +1,5 @@
+package liga.medical.consumermodule.model;
+
+public enum MessageType {
+    DAILY, ALERT, ERROR
+}
